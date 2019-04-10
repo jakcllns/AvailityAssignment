2aff6b64836fa2960d31708565432a480441011f
Jake Collins <jakcllns@gmail.com>
2019-04-09 22:50:56 -0400

    Created function with passing test

--- Question 4/LispValidator.js
+++ Question 4/LispValidator.js
@@ -1,15 +1,23 @@
 function areParenthesesValid(LispCode){
     //Strip out everything except for parentheses
-    
+    let parenthesesOnly = LispCode.replace(/[^\(\)]/g, '');
     //Stack for tracking current open parentheses
-    
+    let stack = [];
     //Verify that start of code is wrapped in open and close parentheses
-    
+    if(LispCode[0] != '(' && LispCode[LispCode.length-1] != ')'){ return false; }
 
     //Traverse through the levels of parentheses nesting to confirm open close pairs
-    
+    for(let i = 0; i < parenthesesOnly.length; i++){
+        if(parenthesesOnly[i] == '('){ 
+            stack.push(parenthesesOnly[i]); 
+        } else {
+            //Check if stack is empty indicating a close parenthesis without a matched open parenthesis
+            if(stack.length == 0){ return false; }
+            stack.pop();
+        }       
+    }
     //Check stack length at end to confirm if there are any extra open parenthesis
-    
+    return stack.length == 0 ? true : false;
 }
 
 //Failing LISP Code snippet
@@ -36,6 +44,6 @@ let goodSnippet = `(defparameter *small* 1)
    (defparameter *big* 100)
    (guess-my-number))`;
 
-console.log('Does the function return a Boolean:\n' + (typeof areParenthesesValid('()') === 'boolean' ? 'Yes': 'No'));
-console.log('This test should result in a false value:\n' + areParenthesesValid(badSnippet));
-console.log('This test should result in a true value:\n' + areParenthesesValid(goodSnippet));
+console.log('Does the function return a Boolean: ' + (typeof areParenthesesValid('()') === 'boolean' ? 'Pass': 'Fail'));
+console.log('This test should result in a false value: ' + (areParenthesesValid(badSnippet) == false ? 'Pass' : 'Fail'));
+console.log('This test should result in a true value: ' + (areParenthesesValid(goodSnippet) == true ? 'Pass' : 'Fail'));
