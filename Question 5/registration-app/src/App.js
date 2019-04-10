3a25e165b4791b218fc91f1cf8fa02b8cbce9e13
Jake Collins <jakcllns@gmail.com>
2019-04-10 19:22:14 -0400

    Planned out the design

--- Question 5/registration-app/src/App.js
+++ Question 5/registration-app/src/App.js
@@ -1,25 +1,25 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
 import './App.css';
+
+//import bootstrap css and Availity css
+
+// add const for list of state abreviations
 
 class App extends Component {
   render() {
+    // Add code for state properties
+
+    // Add event handler for form submit to supply an alert box that the user was able to successfully submit
+
     return (
       <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
+        {/* Create base form */}
+
+        {/* Form must have fields for First Name, Last Name, NPI Number, Business Address,
+          Telephone number, and Email address */}
+
+        {/* Form needs a register button */}
+
       </div>
     );
   }
