c1ca1849329d081831a301657edd15351900607e
Jake Collins <jakcllns@gmail.com>
2019-04-10 00:50:42 -0400

    Added folder for output files and wrote out requirements

---
+++ Question 6/CsvParser.py
@@ -0,0 +1,9 @@
+# Open file as CSV
+
+# Separate file by Insurance Company
+
+# Sort Users in Insurance Company by Last Name
+
+# Only include the most recent version # for duplicate user ids
+
+# Output separete files for each company to the OutputFiles folder
