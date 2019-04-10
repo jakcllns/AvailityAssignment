5f5a9819d44854869abdb31498e2bf31c6c85c0d
Jake Collins <jakcllns@gmail.com>
2019-04-10 12:31:00 -0400

    Before refactoring and adding classes for Question 6

--- Question 6/CsvParser.py
+++ Question 6/CsvParser.py
@@ -1,4 +1,15 @@
+import csv
+import os
+
+BASE_DIR = os.path.dirname(os.path.abspath(__file__))
+COMPANY_COLUMN = 4
+VERSION_COLUMN = 3
+LAST_NAME_COLUMN = 2
+USERID_COLUMN = 0
+
 # Open file as CSV
+def GetCsvFile():
+    return open(BASE_DIR + '\\mockData.csv')
 
 # Separate file by Insurance Company
 
@@ -7,3 +18,32 @@
 # Only include the most recent version # for duplicate user ids
 
 # Output separete files for each company to the OutputFiles folder
+
+def Main():
+    csvData = GetCsvFile()
+    reader = csv.reader(csvData, 'excel')
+    companies = {}
+    index = 0
+    headers = reader.__next__()
+
+    for row in reader:
+        if companies.get(row[-1]) == None:
+            companies[row[-1]] =  {'records':[headers, row], 'userids': { row[0]: 1 }}
+        else:
+            if companies[row[-1]]['userids'].get(row[0]) == None:
+                companies[row[-1]]['records'].append(row)
+                index = len(companies[row[-1]]['records']) - 1
+                companies[row[-1]]['userids'][row[0]] = index
+            else:
+                index = companies[row[-1]]['userids'][row[0]]
+                if companies[row[-1]]['records'][index][3] < row[3]:
+                    companies[row[-1]]['records'][index] = row
+    
+    for key in companies:
+        companies[key]['records'].sort(key=lambda x: x[2], reverse=False)
+        with open(BASE_DIR + '\\OutputFiles\\' + key + '.csv', 'w') as companyFile:
+            writer = csv.writer(companyFile, 'excel')
+            writer.writerows(companies[key]['records'])
+    csvData.close()
+
+Main()
